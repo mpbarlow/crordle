5a76c2a259bbf42e000af4a36eaf8c7648c8b8ac
mpbarlow <matt@barlow.coffee>
2022-03-15 00:11:30 +0000

    better animations, implement board

--- source/BoardSquare.lua
+++ source/BoardSquare.lua
@@ -1,5 +1,7 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/animator"
+import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 
@@ -7,6 +9,10 @@ local letters <const> = {
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
     "T", "U", "V", "W", "X", "Y", "Z"
 }
+
+local rotationMultiplier <const> = 0.5
+local animationDuration <const> = 250
+local snapTimeout <const> = 1000
 
 -- BoardSquare encapsulates a single letter selection.
 -- Rendering is modelled as an endlessly scrolling, wrapping list. The previous, current, and next
@@ -16,6 +22,9 @@ local letters <const> = {
 class('BoardSquare', {
     -- Stores the scroll offset, from 0 to 26 * size.height.
     offset = 0,
+
+    -- Squares that are not yet in play do not render a letter.
+    inPlay = false,
 
     -- Move the offset by `change` to move the scroll window.
     moveOffset = function(self, change)
@@ -38,9 +47,14 @@ class('BoardSquare', {
         self.sprite:markDirty()
     end,
 
+    -- Return the letter index of a given scroll offset.
+    indexOfOffset = function(self, offset)
+        return math.floor(offset / self.size.height) + 1
+    end,
+
     -- Use the current scroll offset to determine how far through the list of letters we are.
     currentIndex = function(self)
-        return math.floor(self.offset / self.size.height) + 1
+        return self:indexOfOffset(self.offset)
     end,
 
     -- Return the index of the previous letter, wrapping back round to the last if needed.
@@ -65,23 +79,79 @@ class('BoardSquare', {
         return currentIndex + 1
     end,
 
-    -- Handle the player pressing up or down to cycle through letters instead of using the crank.
-    handleInput = function(self, button)
+    -- Animate to `moveBy` letters away
+    moveLetter = function(self, moveBy)
         -- This looks a little weird, but the offset is 0-based while currentIndex is 1-based.
-        -- (currentIndex - 1) * height gives us the center offset of the current letter, so we can
-        -- then subtract or add one whole square size to get the center point of the prev/next.
+        -- (currentIndex - 1) * height gives us the top offset of the current letter, so we can
+        -- then subtract or add n whole square sizes to get the top point of the prev/next.
         -- We don't want to use previousIndex because that will wrap, causing an animation from A
         -- backwards to Z to scroll through 26 letters forwards, instead of 1 backwards.
-        if (button == playdate.kButtonUp) then
-            self:createAnimatorTo(((self:currentIndex() - 1) * self.size.height) - self.size.height)
-        elseif (button == playdate.kButtonDown) then
-            self:createAnimatorTo(((self:currentIndex() - 1) * self.size.height) + self.size.height)
+        self:animateTo(((self:currentIndex() - 1) * self.size.height) + (moveBy * self.size.height))
+    end,
+
+    -- Animate to the next letter.
+    moveToNextLetter = function(self)
+        self:moveLetter(1)
+    end,
+
+    -- Animate to the previous letter.
+    moveToPreviousLetter = function(self)
+        self:moveLetter(-1)
+    end,
+
+    -- Handle `change` degrees of crank rotation.
+    handleCranking = function(self, change)
+        -- If we're currently animating to a new selection from a button press, cancel it so the
+        -- crank takes priority.
+        self.animator = nil
+
+        if (self.snapTimer == nil) then
+            -- This timer will track how long it's been since cranking stopped, so after a brief
+            -- pause we can snap the letter into place.
+            self.snapTimer = playdate.timer.new(snapTimeout)
         end
+
+        -- Reset the timer each time the crank moves so we don't time out while still moving.
+        self.snapTimer:reset()
+
+        self:moveOffset(change * rotationMultiplier)
     end,
 
     -- Create a new animator from the current offset to the given value.
-    createAnimatorTo = function(self, to)
-        self.animator = gfx.animator.new(250, self.offset, to)
+    animateTo = function(self, to)
+        self.animator = gfx.animator.new(animationDuration, self.offset, to)
+    end,
+
+    -- Do anything that needs to run on every frame.
+    update = function(self)
+        -- If we've configured an animator, get the next value and update our current offset to it.
+        if (self.animator ~= nil) then
+            -- If we're already animating something, that means that either a button was pressed,
+            -- or our snap timer expired and we're animating it into place. In either case, we no
+            -- longer want the timer to be running.
+            if (self.snapTimer ~= nil) then
+                self.snapTimer:remove()
+                self.snapTimer = nil
+            end
+
+            self:setOffset(self.animator:currentValue())
+
+            if (self.animator:ended()) then
+                self.animator = nil
+            end
+        -- If we have a complete snapTimer, snap the letter into place.
+        elseif (self.snapTimer ~= nil and self.snapTimer.timeLeft == 0) then
+            -- Kill the timer to stop us getting into an infinite loop.
+            self.snapTimer:remove()
+            self.snapTimer = nil
+
+            -- self.offset starts at the top of the square, so the logical snapping point from a
+            -- visual perspective should act as if we were scrolled half a square further than we
+            -- actually are, so line up with the center line.
+            self:animateTo(
+                (self:indexOfOffset(self.offset + (self.size.height / 2)) - 1) * self.size.height
+            )
+        end
     end
 }).extends()
 
@@ -97,36 +167,44 @@ function BoardSquare:init(origin, size)
         -- Draw the border
         gfx.drawRect(0, 0, self.width, self.height)
 
+        -- If the square is not in play it should not render a letter.
+        if (not boardSquare.inPlay) then
+            return
+        end
+
         -- To keep the draw logic simple, we always draw the current, previous, and next letter,
         -- taking advantage of sprite clipping to only show what should be visible.
-        local prevLetter = letters[boardSquare:previousIndex()]
-        local currentLetter = letters[boardSquare:currentIndex()]
-        local nextLetter = letters[boardSquare:nextIndex()]
+        local prevLetter = "*" .. letters[boardSquare:previousIndex()] .. "*"
+        local currentLetter = "*" .. letters[boardSquare:currentIndex()] .. "*"
+        local nextLetter = "*" .. letters[boardSquare:nextIndex()] .. "*"
+
+        local prevWidth = gfx.getTextSize(prevLetter)
+        local currentWidth = gfx.getTextSize(currentLetter)
+        local nextWidth = gfx.getTextSize(nextLetter)
 
         -- getTextSize reports back a height of 20px which doesn't seem to be right...
-        local prevWidth = gfx.getTextSize("*" .. prevLetter .. "*")
-        local currentWidth = gfx.getTextSize("*" .. currentLetter .. "*")
-        local nextWidth = gfx.getTextSize("*" .. nextLetter .. "*")
+        local letterHeight = 16
 
         -- The relative scroll offset within the height of the square.
         local relativeOffset = boardSquare.offset % self.height
 
+        -- Draw the previous, current, and next letters one square height apart.
         gfx.drawText(
-            "*" .. prevLetter .. "*",
-            self.width / 2 - prevWidth / 2,
-            7 - relativeOffset - self.height
+            prevLetter,
+            (self.width / 2) - (prevWidth / 2),
+            (self.height / 2) - (letterHeight / 2) - relativeOffset - self.height
         )
 
         gfx.drawText(
-            "*" .. currentLetter .. "*",
-            self.width / 2 - currentWidth / 2,
-            7 - relativeOffset
+            currentLetter,
+            (self.width / 2) - (currentWidth / 2),
+            (self.height / 2) - (letterHeight / 2) - relativeOffset
         )
 
         gfx.drawText(
-            "*" .. nextLetter .. "*",
-            self.width / 2 - nextWidth / 2,
-            7 - relativeOffset + self.height
+            nextLetter,
+            (self.width / 2) - (nextWidth / 2),
+            (self.height / 2) - (letterHeight / 2) - relativeOffset + self.height
         )
     end
 
--- source/main.lua
+++ source/main.lua
@@ -1,42 +1,73 @@
-import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
 import "BoardSquare"
+import "ActiveRing"
 import "support"
 
 local gfx <const> = playdate.graphics
 
 local wordList <const> = import "words"
-local word
 
-local rotationDivisor <const> = 2
+local boardOrigin <const>  = {x = 10, y = 10}
+local squareSize <const>   = {width = 30, height = 30}
+local squareMargin <const> = 5
 
-local boardSquare = BoardSquare({x = 10, y = 10}, {width = 30, height = 30})
+local board <const> = {}
+local activeSquare  = {row = 1, square = 1}
+
+local function makeBoard()
+    for row = 1, 6 do
+        board[row] = {}
+
+        for square = 1, 5 do
+            local x = ((square - 1) * squareSize.width) + ((square - 1) * squareMargin) + boardOrigin.x
+            local y = ((row - 1) * squareSize.height) + ((row - 1) * squareMargin) + boardOrigin.y
+
+            board[row][square] = BoardSquare({x = x, y = y}, squareSize)
+        end
+    end
+end
+
+makeBoard()
+
+local activeRing = ActiveRing(boardOrigin, squareSize, squareMargin)
+
+local boardSquare = board[activeSquare.row][activeSquare.square]
 
 function playdate.update()
     local change, acceleratedChange = playdate.getCrankChange()
 
     if (acceleratedChange ~= 0) then
-        boardSquare:moveOffset(acceleratedChange / rotationDivisor)
+        boardSquare:handleCranking(acceleratedChange)
+
+    elseif (playdate.buttonJustPressed(playdate.kButtonUp)) then
+        boardSquare:moveToPreviousLetter()
+
+    elseif (playdate.buttonJustPressed(playdate.kButtonDown)) then
+        boardSquare:moveToNextLetter()
+
+    elseif (playdate.buttonJustPressed(playdate.kButtonLeft)) then
+        activeSquare.square -= 1
+        activeRing:moveTo(activeSquare.row, activeSquare.square)
+
+    elseif (playdate.buttonJustPressed(playdate.kButtonRight)) then
+        activeSquare.square += 1
+        activeRing:moveTo(activeSquare.row, activeSquare.square)
     end
 
-    for _, button in pairs({playdate.kButtonUp, playdate.kButtonDown}) do
-        if (playdate.buttonJustPressed(button)) then
-            boardSquare:handleInput(button)
-        end
-    end
-
-    -- If we've configured an animator, get the next value and update our current offset to it.
-    if (boardSquare.animator ~= nil) then
-        boardSquare:setOffset(boardSquare.animator:currentValue())
-
-        if (boardSquare.animator:ended()) then
-            boardSquare.animator = nil
+    for row = 1, activeSquare.row do
+        for square = 1, 5 do
+            board[row][square].inPlay = square <= activeSquare.square
+            board[row][square]:update()
         end
     end
 
     gfx.sprite.update()
+
+    playdate.timer.updateTimers()
     playdate.drawFPS(380, 225)
+
+    boardSquare = board[activeSquare.row][activeSquare.square]
 end
